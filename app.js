280ca5a37ad32086a0d268e0e331f6ffd1a8c2ae
Elliot <elliotmorris115@gmail.com>
2019-05-21 20:12:39 +0100

    Added header/hero

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Hero slider */
+
+$(document).ready(function() {
+  $(".hero__slider").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    prevArrow: false,
+    nextArrow: false
+  });
+});
+
