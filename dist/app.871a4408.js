bc041b11c95cce62436096778981912af79b8808
Elliot <elliotmorris115@gmail.com>
2019-05-21 20:57:36 +0100

    add tabs

--- dist/app.871a4408.js
+++ dist/app.871a4408.js
@@ -118,14 +118,29 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"../app.js":[function(require,module,exports) {
-/* Hero slider */
 $(document).ready(function () {
+  /* Hero slider */
   $(".hero__slider").slick({
     autoplay: true,
     autoplaySpeed: 2000,
     infinite: true,
     prevArrow: false,
     nextArrow: false
+  });
+  /* Tabs */
+
+  $(".tabs-list li a").click(function (e) {
+    e.preventDefault();
+  });
+  $(".tabs-list li").click(function () {
+    var tabid = $(this).find("a").attr("href");
+    $(".tabs-list li,.tabs div.tab").removeClass("active"); // removing active class from tab
+
+    $(".tab").hide(); // hiding open tab
+
+    $(tabid).show(); // show tab
+
+    $(this).addClass("active"); //  adding active class to clicked tab
   });
 });
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
